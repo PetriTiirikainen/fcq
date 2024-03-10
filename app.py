0f4e341cefc0b1bdd0d2940f1e47b1522b003594
Petri Tiirikainen <147138344+PetriTiirikainen@users.noreply.github.com>
2024-03-10 17:37:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from chatbot import chatbot_init, get_chat_response
 google_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 # Parse JSON content
-credentials = json.loads(google_credentials)
+# credentials = json.loads(google_credentials)
 
 # Set the path to the Google credentials file
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ".\\keys\\wbs-final-project-412007-0e822eb70e15.json"
